d4839d7bdb1ac5971b1ecca05e661197b90cf37c
Kamila Nagorska <gdyniakan@op.pl>
2020-11-22 16:13:06 +0100

    done almost

---
+++ Laboratorium2oraz3/accounts/authentication.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
+
+#umożliwia zalogowanie się za pomocą e-maila, a nie nazwy użytkownika
+
+class EmailAuthBackend(object):
+    """
+    Authenticate using an e-mail address.
+    """
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
