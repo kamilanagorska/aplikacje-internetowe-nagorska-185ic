49635ead6cc46a6e1e60c4057ff15696bcdacb27
Kamila Nagorska <gdyniakan@op.pl>
2021-01-12 01:33:53 +0100

    some changes

--- Laboratorium9/food/settings.py
+++ Laboratorium9/food/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=2ieq#v@ji9!=wj=1d1ffmn01cl0x589#(kvk!=%+%b83d-u2s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
