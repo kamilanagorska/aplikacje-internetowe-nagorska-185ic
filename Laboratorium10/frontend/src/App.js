56312ed9c3dcdfa6697d8254673659aa63beed94
Kamila Nagorska <gdyniakan@op.pl>
2021-01-20 14:07:07 +0100

    readme time

--- Laboratorium10/frontend/src/App.js
+++ Laboratorium10/frontend/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
-import { Card, Button, CardHeader, CardBody } from 'reactstrap';
+import { Card, Button, CardHeader, CardBody, ButtonGroup } from 'reactstrap';
 import { ListGroup, ListGroupItem, Badge, FormText } from 'reactstrap';
-import moment from 'react-moment';
 
     class App extends Component {
       constructor(props) {
         super(props);
         this.state = {
           viewDone: false,
+          sorted: false,
+          order: "",
 
           activeItem: {
             title: "",
@@ -18,7 +19,7 @@ import moment from 'react-moment';
             date: "",
             done: false
           },
-          plannedList: []
+          plannedList: [],
         };
       }
       componentDidMount() {
@@ -33,10 +34,49 @@ import moment from 'react-moment';
       
       displayDone = status => {
         if (status) {
-          return this.setState({ viewDone: true });
+          return this.setState({ viewDone: true});
         }
-        return this.setState({ viewDone: false });
+        return this.setState({ viewDone: false});
       };
+
+      sortUp = property => {
+        return function(a,b){
+          if(a[property] > b[property])
+          return 1;
+          else if(a[property] < b[property])
+          return -1;
+
+          return 0;
+        }
+      }
+
+      sortDown = property => {
+        return function(a,b){
+          if(a[property] < b[property])
+          return 1;
+          else if(a[property] > b[property])
+          return -1;
+
+          return 0;
+        }
+      }
+      
+
+      sortList = (property, order) => {  
+          if(order){
+            this.state.plannedList.sort(this.sortUp(property))
+            this.setState({
+              sorted: true,
+              order: order
+            })
+          } else {
+            this.state.plannedList.sort(this.sortDown(property))
+            this.setState({
+              sorted: true,
+              order: order
+            })
+          }
+      }
 
       renderTabList = () => {
         return (
@@ -56,11 +96,16 @@ import moment from 'react-moment';
               >
                 not done
             </Button>
+            <ButtonGroup>
+              <Button outline color="secondary" size="sm" style={{marginLeft:'10px'}} onClick={() => this.sortList("date", true)}>sort by date ↑</Button>
+              <Button outline color="secondary" size="sm" onClick={() => this.sortList("date", false)}>sort by date ↓</Button>
+            </ButtonGroup>
           </CardHeader>
         );
       };
 
-      checkDate = item => {
+
+      checkTodayDate = item => {
         const itemDate = item.date;
         const today = new Date();
         const d = today.getDate();
@@ -70,18 +115,33 @@ import moment from 'react-moment';
         }
         const y = today.getFullYear();
         const check = `${y}-${m}-${d}`;
-        console.log(check);
-        console.log(itemDate);
         if (itemDate === check){
           return true;
         } else {
           return false;
         }
       }
+
+      checkPast = item => {
+        const itemDate = item.date;
+        const today = new Date();
+        const d = today.getDate();
+        let m = today.getMonth() +1;
+        if (m < 10){
+          m = `0${m}`;
+        }
+        const y = today.getFullYear();
+        const dateToCheck = itemDate.split("-");
+        if (y < dateToCheck[0] || m < dateToCheck[1] || d < dateToCheck[2]){
+          return false;
+        } else {
+          return true;
+        }
+      }
       renderItems = () => {
-        const { viewDone: viewDone} = this.state;
+        const { viewDone: done} = this.state;
         const newItems = this.state.plannedList.filter(
-          item => item.done === viewDone
+          item => item.done === done,
         );
         return newItems.map(item => (
           <ListGroupItem key={item.id}>
@@ -95,8 +155,9 @@ import moment from 'react-moment';
               <FormText color="muted">
                 {item.description}
               </FormText>
-              {this.checkDate(item) ? 
+              {this.checkTodayDate(item) ? 
               (<Badge color="danger">today</Badge>) 
+              : this.checkPast(item) ? <Badge color="warning">too late</Badge>
               : <Badge color="light">{item.date}</Badge>
               }
             </span>
@@ -136,12 +197,14 @@ import moment from 'react-moment';
       editItem = item => {
         this.setState({ activeItem: item, modal: !this.state.modal });
       };
+
       render() {
         return (
           <main className="font2">
             <br></br>
             <Card style={{width:'60%', margin: '0 auto'}}>
               <CardHeader className="font" style={{fontSize:'50px', textAlign: 'center', paddingTop: '10px'}}>Planner<br></br>
+              <p className="font2" style={{fontSize:'30px'}}>{new Date().toLocaleDateString()}</p>
                 <Button outline color="info" className="font2" onClick={this.createItem} style={{width:'10%'}}>Add new</Button>
               </CardHeader>
               <CardBody>
